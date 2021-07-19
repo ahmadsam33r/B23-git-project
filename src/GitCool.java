b14d5c916326bd24156baee3469ab3f0ba8963d8
ahmadsam33r <86983918+ahmadsam33r@users.noreply.github.com>
2021-07-19 13:43:57 -0700

    added github account

---
+++ src/GitCool.java
@@ -0,0 +1,8 @@
+public class GitCool {
+    // B23 is learining Git
+    // B23 is a special number
+    // B23 is making some sense
+
+    // we have added our github account
+    // we were able to suceesfully .....
+}
